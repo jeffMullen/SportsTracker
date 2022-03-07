01351aa8451d67d581ba1d96d44e1aec52b8c0f1
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-03-06 21:13:12 -0500

    viewModel receiving the sport id when sport card is clicked

--- app/src/main/java/com/android/sportstracker/ui/MainActivity.kt
+++ app/src/main/java/com/android/sportstracker/ui/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.android.sportstracker.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.fragment.NavHostFragment
-import com.android.sportstracker.R
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModelProvider
 import com.android.sportstracker.databinding.ActivityMainBinding
+import com.android.sportstracker.viewModels.SportPickerViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sportPickerViewModel: SportPickerViewModel
+//    private val mLifecycleRegistry mLifecycleRegistry
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        Log.i("VIEW MODEL", "Called ViewModelProvider.get()")
+        sportPickerViewModel = ViewModelProvider(this).get(SportPickerViewModel::class.java)
         setContentView(binding.root)
 
+        binding.baseballCard.setOnClickListener { setSearchString(binding.baseballText) }
+        binding.footballCard.setOnClickListener { setSearchString(binding.footballText) }
+        binding.hockeyCard.setOnClickListener { setSearchString(binding.hockeyText) }
+        binding.basketballCard.setOnClickListener { setSearchString(binding.basketballText) }
+        binding.soccerCard.setOnClickListener { setSearchString(binding.soccerText) }
+
+    }
+
+    fun setSearchString(view: TextView) {
+        val sport = view.text
+        sportPickerViewModel.setSport(sport as String)
+        val returnedSport: String? = sportPickerViewModel.getSport()
+        Log.i("VIEWMODEL RETURN SPORT", "${returnedSport}")
 
     }
 }
--- app/src/main/java/com/android/sportstracker/viewModels/SportPickerViewModel.kt
+++ app/src/main/java/com/android/sportstracker/viewModels/SportPickerViewModel.kt
@@ -1,7 +1,38 @@
 package com.android.sportstracker.viewModels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class SportPickerViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private lateinit var sportId: String
+
+    fun setSport(sport: String) {
+
+        if (sport == "Baseball") {
+            this.sportId = "MLB"
+            Log.d("BASEBALL", sportId)
+        } else if (sport == "Football"){
+            this.sportId = "NFL"
+            Log.d("FOOTBALL", sportId)
+        } else if (sport == "Hockey") {
+            this.sportId = "NHL"
+            Log.d("HOCKEY", sportId)
+        } else if (sport == "Basketball") {
+            this.sportId = "NBA"
+            Log.d("BASKETBALL", sportId)
+        } else if (sport == "Soccer") {
+            this.sportId = "American%20Major%20League%20Soccer"
+            Log.d("SOCCER", sportId)
+        }
+    }
+
+
+    fun getSport(): String { return this.sportId }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("SPORTPICKER", "SportPickerViewModel onCleared()!!!!")
+    }
+
 }
